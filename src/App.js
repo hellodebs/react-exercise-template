84ce7989f51e040e8789242dc7016e4072a07e11
hellodebs <debora.behrens@outlook.com>
2021-07-19 15:19:57 +0200

    Add addeventlistener

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ import "./App.css";
 function App() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   // Your code below
+  useEffect(() => {
+    //create function
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+    }
+    //add addEventListener
+    window.addEventListener("resize", handleResize);
+
+    //clean up function
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <div className="App">
