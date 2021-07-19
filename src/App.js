cc9fe98ed0dda24b2e5ac9f80c61b0e04136898b
hellodebs <debora.behrens@outlook.com>
2021-07-19 14:52:50 +0200

    Change doument title to hello neuefische

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [message, setMessage] = useState("Hello React");
   // Your code below
+  useEffect(() => {
+    document.title = "Hello neuefische ";
+  });
 
   return (
     <div className="App">
